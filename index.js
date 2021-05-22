f5b0a0506b576df046871b358a039716e524a7c5
aurghya-0 <aurghya.blog@gmail.com>
2021-05-22 10:16:57 +0530

    removed useless code

--- index.js
+++ index.js
@@ -10,25 +10,4 @@ client.events = new Discord.Collection();
     require(`./handlers/${handler}`)(client, Discord);
 })
 
-// const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-// for (const file of commandFiles) {
-//     const command = require(`./commands/${file}`);
-//     client.commands.set(command.name, command);
-// }
-
-// client.on('message', (message) => {
-//     if (!message.content.startsWith(prefix) || message.author.bot) return;
-//     const args = message.content.slice(prefix.length).split(/ +/);
-//     const command = args.shift().toLowerCase();
-
-//     if (command === 'ping') {
-//         client.commands.get('ping').execute(message, args);
-//     }
-// });
-
-// client.once('ready', () => {
-//     console.log('Bot is logged in')
-// });
-
-
 client.login(key.clienToken);
